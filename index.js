7dfe0d38bcdcdd29b28eebcc0fd483295659a143
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2019-04-03 19:24:31 +0200

    test sdk event slack

--- index.js
+++ index.js
@@ -6,60 +6,80 @@ var SLACK_VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN;
 
 var express = require('express');
 var request = require('request');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/slack/slash-commands/send-me-buttons', (req, res) => {
-  // res.status(200).end() // best practice to respond with empty 200 status code
-  console.log(req.body);
-  var reqBody = req.body
-  var responseURL = reqBody.response_url
-  if (reqBody.token !== SLACK_VERIFICATION_TOKEN) {
-    res.status(403).end("Access forbidden")
-  } else {
-    console.log("Access granted")
-    return res.json(
-      {
-        "type": "actions",
-        "elements": [
-          {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Explore",
-              "emoji": true
-            },
-            "url": "https://docs.google.com/document/d/1Lr6Zn65WOx7ju2pvjkKSMrhCjeERzAGHDSIjrOO1nj4/edit"
-          }
-        ]
-      }
-    );
-    // sendMessageToSlackResponseURL(responseURL, message)
-  }
-})
+// Initialize using signing secret from environment variables
+const { createEventAdapter } = require('@slack/events-api');
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+const port = process.env.PORT || 3000;
 
-function sendMessageToSlackResponseURL(responseURL, JSONmessage) {
-  console.log("send response to " + responseURL);
-  var postOptions = {
-    uri: responseURL,
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    json: JSONmessage
-  }
-  request(postOptions, (error, response, body) => {
-    console.log("slack response: ");
-    console.log(response);
-    console.log("slack body: " + body);
-    if (error) {
-      // handle errors as you see fit
-      console.log(error)
-    }
-  })
-}
+// Mount the event handler on a route
+// NOTE: you must mount to a path that matches the Request URL that was configured earlier
+app.use('/slack/events', slackEvents.expressMiddleware());
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event)=> {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
+
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
+
+
+
+//
+// app.post('/slack/slash-commands/send-me-buttons', (req, res) => {
+//   // res.status(200).end() // best practice to respond with empty 200 status code
+//   console.log(req.body);
+//   var reqBody = req.body
+//   var responseURL = reqBody.response_url
+//   if (reqBody.token !== SLACK_VERIFICATION_TOKEN) {
+//     res.status(403).end("Access forbidden")
+//   } else {
+//     console.log("Access granted")
+//     return res.json(
+//       {
+//         "type": "actions",
+//         "elements": [
+//           {
+//             "type": "button",
+//             "text": {
+//               "type": "plain_text",
+//               "text": "Explore",
+//               "emoji": true
+//             },
+//             "url": "https://docs.google.com/document/d/1Lr6Zn65WOx7ju2pvjkKSMrhCjeERzAGHDSIjrOO1nj4/edit"
+//           }
+//         ]
+//       }
+//     );
+//     // sendMessageToSlackResponseURL(responseURL, message)
+//   }
+// })
+//
+// function sendMessageToSlackResponseURL(responseURL, JSONmessage) {
+//   console.log("send response to " + responseURL);
+//   var postOptions = {
+//     uri: responseURL,
+//     method: 'POST',
+//     headers: {
+//       'Content-type': 'application/json'
+//     },
+//     json: JSONmessage
+//   }
+//   request(postOptions, (error, response, body) => {
+//     console.log("slack response: ");
+//     console.log(response);
+//     console.log("slack body: " + body);
+//     if (error) {
+//       // handle errors as you see fit
+//       console.log(error)
+//     }
+//   })
+// }
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
